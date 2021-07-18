9f7c63f22a2a6737bdf27217da88830b6347b811
IronBlazeJS <403210815@qq.com>
2021-07-18 21:08:39 +0800

    增加异常处理

---
+++ rpc-common/src/main/java/com/matty/rpc/exception/RpcException.java
@@ -0,0 +1,24 @@
+package com.matty.rpc.exception;
+
+import com.matty.rpc.enumeration.RpcError;
+
+/**
+ * ClassName: RpcException
+ * author: Matty Roslak
+ * date: 2021/7/18  19:04
+ * Rpc调用异常
+ */
+public class RpcException extends RuntimeException {
+
+    public RpcException(RpcError error, String detail){
+        super(error.getMessage() + ":" + detail);
+    }
+
+    public RpcException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public RpcException(RpcError error){
+        super(error.getMessage());
+    }
+}
