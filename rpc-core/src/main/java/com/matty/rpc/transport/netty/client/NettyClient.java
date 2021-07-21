2a7f36551eb2d968af8e2130b24db2a4d4a0e5c6
IronBlazeJS <403210815@qq.com>
2021-07-21 20:24:41 +0800

    统一管理Netty客户端请求

--- rpc-core/src/main/java/com/matty/rpc/transport/netty/client/NettyClient.java
+++ rpc-core/src/main/java/com/matty/rpc/transport/netty/client/NettyClient.java
@@ -4,23 +4,22 @@ import com.matty.rpc.entity.RpcRequest;
 import com.matty.rpc.entity.RpcResponse;
 import com.matty.rpc.enumeration.RpcError;
 import com.matty.rpc.exception.RpcException;
+import com.matty.rpc.factory.SingletonFactory;
 import com.matty.rpc.register.NacosServiceDiscovery;
 import com.matty.rpc.register.ServiceDiscovery;
 import com.matty.rpc.serializer.CommonSerializer;
 import com.matty.rpc.transport.RpcClient;
-import com.matty.rpc.util.RpcMessageChecker;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * ClassName: NettyClient
@@ -39,13 +38,14 @@ public class NettyClient implements RpcClient {
         group = new NioEventLoopGroup();
         bootstrap = new Bootstrap();
         bootstrap.group(group)
-                .channel(NioSocketChannel.class)
-                .option(ChannelOption.SO_KEEPALIVE, true);
+                .channel(NioSocketChannel.class);
     }
 
     private final ServiceDiscovery serviceDiscovery;
 
     private final CommonSerializer serializer;
+
+    private final UnprocessedRequests unprocessedRequests;
 
     public NettyClient() {
         //以默认序列化器调用构造函数
@@ -55,17 +55,17 @@ public class NettyClient implements RpcClient {
     public NettyClient(Integer serializerCode) {
         serviceDiscovery = new NacosServiceDiscovery();
         serializer = CommonSerializer.getByCode(serializerCode);
+        unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
     }
 
     @Override
-    public Object sendRequest(RpcRequest rpcRequest) {
+    public CompletableFuture<RpcResponse> sendRequest(RpcRequest rpcRequest) {
         if (serializer == null) {
             logger.error("未设置序列化器");
             throw new RpcException(RpcError.SERIALIZER_NOT_FOUND);
         }
 
-        //保证自定义实体类变量的原子性和共享性的线程安全，此处应用于rpcResponse
-        AtomicReference<Object> result = new AtomicReference<>(null);
+        CompletableFuture<RpcResponse> resultFuture = new CompletableFuture<>();
 
         try {
             //从Nacos获取提供对应服务的服务端地址
@@ -76,28 +76,28 @@ public class NettyClient implements RpcClient {
                 group.shutdownGracefully();
                 return null;
             }
+
+            //将新请求放入未处理完的请求中
+            unprocessedRequests.put(rpcRequest.getRequestId(), resultFuture);
+
             //向服务端发请求，并设置监听，关于writeAndFlush()的具体实现可以参考：https://blog.csdn.net/qq_34436819/article/details/103937188
-            channel.writeAndFlush(rpcRequest).addListener(future1 -> {
+            channel.writeAndFlush(rpcRequest).addListener((ChannelFutureListener) future1 -> {
                 if (future1.isSuccess()) {
                     logger.info(String.format("客户端发送消息：%s", rpcRequest.toString()));
                 } else {
-                    logger.error("发送消息时有错误发生:", future1.cause());
+                    future1.channel().close();
+                    resultFuture.completeExceptionally(future1.cause());
+                    logger.error("发送消息时有错误发生: ", future1.cause());
                 }
             });
-            channel.closeFuture().sync();
-            //AttributeMap<AttributeKey, AttributeValue>是绑定在Channel上的，可以设置用来获取通道对象
-            AttributeKey<RpcResponse> key = AttributeKey.valueOf("rpcResponse" + rpcRequest.getRequestId());
-            //get()阻塞获取value
-            RpcResponse rpcResponse = channel.attr(key).get();
-            RpcMessageChecker.check(rpcRequest, rpcResponse);
-            result.set(rpcResponse.getData());
-
-        } catch (InterruptedException e) {
-            logger.error("发送消息时有错误发生:", e);
+        } catch (Exception e) {
+            //将请求从请求集合中移除
+            unprocessedRequests.remove(rpcRequest.getRequestId());
+            logger.error(e.getMessage(), e);
             //interrupt()这里作用是给受阻塞的当前线程发出一个中断信号，让当前线程退出阻塞状态，好继续执行然后结束
             Thread.currentThread().interrupt();
         }
-        return result.get();
+        return resultFuture;
     }
 
 }
--- rpc-core/src/main/java/com/matty/rpc/transport/netty/client/NettyClientHandler.java
+++ rpc-core/src/main/java/com/matty/rpc/transport/netty/client/NettyClientHandler.java
@@ -2,6 +2,7 @@ package com.matty.rpc.transport.netty.client;
 
 import com.matty.rpc.entity.RpcRequest;
 import com.matty.rpc.entity.RpcResponse;
+import com.matty.rpc.factory.SingletonFactory;
 import com.matty.rpc.serializer.CommonSerializer;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFutureListener;
@@ -9,7 +10,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
-import io.netty.util.AttributeKey;
 import io.netty.util.ReferenceCountUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +25,12 @@ import java.net.InetSocketAddress;
 public class NettyClientHandler extends SimpleChannelInboundHandler<RpcResponse> {
 
     private static final Logger logger = LoggerFactory.getLogger(NettyClientHandler.class);
+
+    private final UnprocessedRequests unprocessedRequests;
+
+    public NettyClientHandler() {
+        unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
+    }
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
@@ -50,11 +56,8 @@ public class NettyClientHandler extends SimpleChannelInboundHandler<RpcResponse>
     protected void channelRead0(ChannelHandlerContext ctx, RpcResponse msg) throws Exception {
         try {
             logger.info(String.format("客户端接收到消息：%s", msg));
-            AttributeKey<RpcResponse> key = AttributeKey.valueOf("rpcResponse" + msg.getRequestId());
-            //将RpcResponse对象以 key 为 rpcResponse + 请求id 放入 ChannelHandlerContext 中
-            ctx.channel().attr(key).set(msg);
-            //关闭客户端通道
-            ctx.channel().close();
+            //将响应数据取出
+            unprocessedRequests.complete(msg);
         } finally {
             ReferenceCountUtil.release(msg);
         }
